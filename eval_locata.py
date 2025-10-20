e1482d791d21b7bded1c2acd92363898ec2b2cae
huwcheston <hwc31@cam.ac.uk>
2025-10-20 19:17:11 +0100

    update csv files

--- eval_locata.py
+++ eval_locata.py
@@ -1,6 +1,7 @@
 import cls_feature_class
 import parameters
 import sys
+import csv
 import os
 import torch
 from time import gmtime, strftime
@@ -15,17 +16,19 @@ def main(args):
     task_id = "3"
     params = parameters.get_params(task_id)
 
-    model_path, out_name = args
+    print(args)
+    _, model_path, out_name = args
 
     # Update all parameters with hardcoded filepaths
     params["dataset_dir"] = Path("./LOCATA_dcase").resolve()
     lab_path = Path("./LOCATA_dcase/feat_label").resolve()
+    results_path = Path("./LOCATA_dcase/results")
     params["feat_label_dir"] = lab_path
     params["model_dir"] = Path("./LOCATA_dcase/models").resolve()
-    params["dcase_output_dir"] = Path("./LOCATA_dcase/results").resolve()
+    params["dcase_output_dir"] = results_path.resolve()
 
     # Extract features if not done already
-    if not len(lab_path.rglob("*.npy")) > 0:
+    if not len(list(lab_path.rglob("*.npy"))) > 0:
         dev_feat_cls = cls_feature_class.FeatureClass(params)
 
         # # Extract features and normalize them
@@ -37,12 +40,11 @@ def main(args):
 
     # Create test dataset
     data_gen_test = DataGenerator(
-        params=params, split=2, shuffle=False, per_file=True
+        params=params, split=1, shuffle=False, per_file=True
     )
 
     # Define device as whatever is available
-    use_cuda = torch.cuda.is_available()
-    device = torch.device("cuda" if use_cuda else "cpu")
+    device = "cpu"
 
     # Initialize evaluation metric class
     score_obj = ComputeSELDResults(params)
@@ -60,23 +62,32 @@ def main(args):
 
     test_loss = test_epoch(data_gen_test, model, criterion, dcase_output_test_folder, params, "cpu")
 
+    # Update csv files
+    for file in Path(dcase_output_test_folder).rglob(".csv"):
+        reader = csv.reader(file, delimiter=',')
+        out = []
+        for row in reader:
+            if row[1] == 1:
+                row = [row[0], 0, *row[2:]]
+            out.append(row)
+
+        writer = csv.writer(file)
+        writer.writerows(out)
+
     use_jackknife = True
     test_ER, test_F, test_LE, test_LR, test_seld_scr, classwise_test_scr = score_obj.get_SELD_Results(dcase_output_test_folder, is_jackknife=use_jackknife )
     print('\nTest Loss')
-    print('SELD score (early stopping metric): {:0.2f} {}'.format(test_seld_scr[0] if use_jackknife else test_seld_scr, '[{:0.2f}, {:0.2f}]'.format(test_seld_scr[1][0], test_seld_scr[1][1]) if use_jackknife else ''))
-    print('SED metrics: Error rate: {:0.2f} {}, F-score: {:0.1f} {}'.format(test_ER[0]  if use_jackknife else test_ER, '[{:0.2f}, {:0.2f}]'.format(test_ER[1][0], test_ER[1][1]) if use_jackknife else '', 100* test_F[0]  if use_jackknife else 100* test_F, '[{:0.2f}, {:0.2f}]'.format(100* test_F[1][0], 100* test_F[1][1]) if use_jackknife else ''))
-    print('DOA metrics: Localization error: {:0.1f} {}, Localization Recall: {:0.1f} {}'.format(test_LE[0] if use_jackknife else test_LE, '[{:0.2f} , {:0.2f}]'.format(test_LE[1][0], test_LE[1][1]) if use_jackknife else '', 100*test_LR[0]  if use_jackknife else 100*test_LR,'[{:0.2f}, {:0.2f}]'.format(100*test_LR[1][0], 100*test_LR[1][1]) if use_jackknife else ''))
     if params['average']=='macro':
         print('Classwise results on unseen test data')
         print('Class\tER\tF\tLE\tLR\tSELD_score')
-        for cls_cnt in range(params['unique_classes']):
-            print('{}\t{:0.2f} {}\t{:0.2f} {}\t{:0.2f} {}\t{:0.2f} {}\t{:0.2f} {}'.format(
-                 cls_cnt,
-                 classwise_test_scr[0][0][cls_cnt] if use_jackknife else classwise_test_scr[0][cls_cnt], '[{:0.2f}, {:0.2f}]'.format(classwise_test_scr[1][0][cls_cnt][0], classwise_test_scr[1][0][cls_cnt][1]) if use_jackknife else '',
-                 classwise_test_scr[0][1][cls_cnt] if use_jackknife else classwise_test_scr[1][cls_cnt], '[{:0.2f}, {:0.2f}]'.format(classwise_test_scr[1][1][cls_cnt][0], classwise_test_scr[1][1][cls_cnt][1]) if use_jackknife else '',
-                 classwise_test_scr[0][2][cls_cnt] if use_jackknife else classwise_test_scr[2][cls_cnt], '[{:0.2f}, {:0.2f}]'.format(classwise_test_scr[1][2][cls_cnt][0], classwise_test_scr[1][2][cls_cnt][1]) if use_jackknife else '',
-                 classwise_test_scr[0][3][cls_cnt] if use_jackknife else classwise_test_scr[3][cls_cnt], '[{:0.2f}, {:0.2f}]'.format(classwise_test_scr[1][3][cls_cnt][0], classwise_test_scr[1][3][cls_cnt][1]) if use_jackknife else '',
-                 classwise_test_scr[0][4][cls_cnt] if use_jackknife else classwise_test_scr[4][cls_cnt], '[{:0.2f}, {:0.2f}]'.format(classwise_test_scr[1][4][cls_cnt][0], classwise_test_scr[1][4][cls_cnt][1]) if use_jackknife else ''))
+        cls_cnt = 0
+        print('{}\t{:0.2f} {}\t{:0.2f} {}\t{:0.2f} {}\t{:0.2f} {}\t{:0.2f} {}'.format(
+             cls_cnt,
+             classwise_test_scr[0][0][cls_cnt] if use_jackknife else classwise_test_scr[0][cls_cnt], '[{:0.2f}, {:0.2f}]'.format(classwise_test_scr[1][0][cls_cnt][0], classwise_test_scr[1][0][cls_cnt][1]) if use_jackknife else '',
+             classwise_test_scr[0][1][cls_cnt] if use_jackknife else classwise_test_scr[1][cls_cnt], '[{:0.2f}, {:0.2f}]'.format(classwise_test_scr[1][1][cls_cnt][0], classwise_test_scr[1][1][cls_cnt][1]) if use_jackknife else '',
+             classwise_test_scr[0][2][cls_cnt] if use_jackknife else classwise_test_scr[2][cls_cnt], '[{:0.2f}, {:0.2f}]'.format(classwise_test_scr[1][2][cls_cnt][0], classwise_test_scr[1][2][cls_cnt][1]) if use_jackknife else '',
+             classwise_test_scr[0][3][cls_cnt] if use_jackknife else classwise_test_scr[3][cls_cnt], '[{:0.2f}, {:0.2f}]'.format(classwise_test_scr[1][3][cls_cnt][0], classwise_test_scr[1][3][cls_cnt][1]) if use_jackknife else '',
+             classwise_test_scr[0][4][cls_cnt] if use_jackknife else classwise_test_scr[4][cls_cnt], '[{:0.2f}, {:0.2f}]'.format(classwise_test_scr[1][4][cls_cnt][0], classwise_test_scr[1][4][cls_cnt][1]) if use_jackknife else ''))
 
 
 if __name__ == "__main__":
